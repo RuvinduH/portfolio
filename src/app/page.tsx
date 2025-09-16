94a2e9ab74cee99fde4198a439983a3a1a37084a
RuvinduH <104176123+RuvinduH@users.noreply.github.com>
2025-09-16 19:01:47 +1000

    footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <TapeSection />
       <AboutSection />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
